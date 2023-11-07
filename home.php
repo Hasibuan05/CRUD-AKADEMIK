f4f9459a3c22d97f6ba969a8658cbde4c18605ab
Lola hasibuan <96487932+Hasibuan05@users.noreply.github.com>
2023-11-07 11:48:39 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,7 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+  	
+    <h1 class="display-4">SELAMAT DATANG DI ITB INDONESIA</h1>
+    <img src="kampus.jpg" width="500" height="400">
+  </div>
+</div>
